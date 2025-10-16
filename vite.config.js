3447b01ba3600c3576ca5cbc789f603121406821
Mustafa Enes Öztürk <ozturkmustafaenes10@gmail.com>
2025-10-17 01:31:22 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/colorpicker/', // 🔥 GitHub Pages alt dizin yolu
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-  }
+  },
 })
